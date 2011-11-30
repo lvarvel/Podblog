966c5d36a53049ea5cab98402efa052fddf657a4
L. David Varvel <megaduck@gmail.com>
2011-11-30 13:00:15 -0800

    Comments are good.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
+#  Posts put the "blog" in "podblog".
+#
+#  Posts have the following properties:
+#    title - The title of the post.
+#    body - The body text.
+#
+#  Plus, as always, the obligatory timestamps.
+
   belongs_to :user
   has_one :podcast
 end
